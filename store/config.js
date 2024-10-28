2b717ad44904f86beb2820a224eddc6ffe86be90
小莫唐尼 <1431128779@qq.com>
2024-10-28 18:25:40 +0800

    update: 修改模拟数据请求地址为动态配置

--- store/config.js
+++ store/config.js
@@ -7,6 +7,7 @@
  */
 import {DefaultAppConfigs, getAppConfigs, setAppConfigs,setAppMockJson,getAppMockJson} from '@/config/index.js'
 import v2Config from '@/api/v2/all.config.js'
+import utils from '@/utils/index.js'
 
 export default {
     namespaced: true,
@@ -59,8 +60,7 @@ export default {
         // 请求模拟数据
         fetchMockJson({state, commit, dispatch}) {
             return new Promise(async (resolve, reject) => {
-                // const mockJsonUrl = utils.checkUrl(state.configs.basicConfig.auditModeMock)
-                const mockJsonUrl = "https://uni-halo.925i.cn/unihalo.mock.json"
+                const mockJsonUrl = utils.checkUrl(state.configs.basicConfig.auditModeMockUrl)
                 uni.request({
                     url: mockJsonUrl,
                     method: "GET",

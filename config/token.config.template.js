552827eaac3cfbe8edef7ce78ddf11315752dc1c
小莫唐尼 <1431128779@qq.com>
2025-06-13 12:47:12 +0800

    update: 调整token配置参数

--- config/token.config.template.js
+++ config/token.config.template.js
@@ -2,14 +2,14 @@ import {getAppConfigs} from "@/config/index";
 
 /** 配置后台管理员token */
 const HaloTokenConfig = Object.freeze({
-	
+
 	/** 基础请求域名：你的Halo博客基础域名 */
-	BASE_API: "https://demo.halo.run",
+	BASE_API: "",
 	// BASE_API: "https://blog.xiaoxiaomo.cn",
 
 
 	/** 管理员token */
-	systemToken: getAppConfigs()?.basicConfig?.personalToken,
+	systemToken: getAppConfigs()?.basicConfig?.tokenConfig?.personalToken,
 	/** 匿名用户token */
 	anonymousToken: ``
 })

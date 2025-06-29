4246d8cdcbb94d6f6046f8fce9505f0f2ea03c70
小莫唐尼 <tzxiaomo@outlook.com>
2025-06-29 11:29:06 +0800

    refactor: 调整默认的配置文件

--- config/token.config.js
+++ config/token.config.js
@@ -1,12 +1,14 @@
 import {getAppConfigs} from "@/config/index";
 
-/** 配置博客的内容 */
+/** 应用配置 */
 const UniHaloConfig = Object.freeze({
 
 	/** 基础请求域名：你的Halo博客基础域名 */
 	BASE_API: "",
+	// 比如：
 	// BASE_API: "https://blog.xiaoxiaomo.cn",
 
+	// token
 	systemToken: getAppConfigs()?.basicConfig?.tokenConfig?.personalToken,
 })
 
